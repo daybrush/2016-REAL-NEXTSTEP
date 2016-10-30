b841bb4fc36375bec8261d10a7cb572adeea0d7c
Daybrush <itoolsg@gmail.com>
2016-10-31 04:12:05 +0900

    add highlighter

--- src/js/custommarked.js
+++ src/js/custommarked.js
@@ -2,6 +2,8 @@ import marked from 'marked';
 
 
 export default function custommarked(mark) {
+	//highlighter
+	mark = mark.replace(/```(\S+)([^```]+)```/g, "<pre><code class='$1'>$2</code></pre>");
 	
 	
 	return marked(mark);
