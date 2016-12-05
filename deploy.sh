b5ed1766c03a3f781bd695d794b08fbd98784d37
Daybrush <itoolsg@gmail.com>
2016-12-05 21:43:50 +0900

    travis CI

--- deploy.sh
+++ deploy.sh
@@ -28,6 +28,7 @@ cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
+
 # Clean out existing contents
 rm -rf out/**/* || exit 0
 
@@ -47,12 +48,20 @@ ssh-add deploy_key
 # Now let's go have some fun with the cloned repo
 cd out
 
+
+ 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 #if [ -z `git diff --exit-code` ]; then
 #    echo "No changes to the output on this push; exiting."
 #    exit 0
 #fi
 
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+ 
+ 
+ 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
